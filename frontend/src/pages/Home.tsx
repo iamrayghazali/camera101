77d2fbc66fee5274531a6837f3a7ef9754ae12ca
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-09-17 12:43:41 +0200

    Changed structure for auth users and unauth users

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,25 @@
+import {Link} from "react-router-dom";
+import {useAuth} from "../hooks/UseAuth.tsx";
 
 export default function Home() {
+    const { isAuthenticated } = useAuth();
+
   return (
-    <main className="bg-secondary max-w-4xl mx-auto p-6 font-fira">
-      <h1 className="text-4xl font-bold mb-4">Welcome</h1>
-      <p className="text-lg text-slate-700">
-        This is the simple Home page.
-      </p>
-    </main>
+      <>
+              <main id="bg-img" className={`flex flex-col justify-center items-center w-full h-screen font-fira `}>
+          { isAuthenticated ? (
+              <>
+              {/*TODO SHOW STATS OF COURSES*/}
+              </>
+          ) : (
+              <>
+                  <h1 className=" text-black font-black text-5xl md:text-6xl p-5 text-center font-boldie leading-relaxed ">Take Your <br/><span className="  leaves-bg hollow-text leading-relaxed text-green-600 text-5xl md:text-7xl">CAMERA SKILLS</span><br/> to the Next Level.</h1>
+                  <Link to="" className="mt-20"> {/*//TODO*/}
+                      <a className=" rounded-xl leaves-bg px-5 py-2 text-2xl text-white font-bold">Start Learning</a>
+                  </Link>
+              </>
+          )}
+              </main>
+      </>
   );
 }
