fde0258d364a59de6f837904c0461a14c30588e5
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-09-17 12:42:27 +0200

    Added new routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,11 +6,15 @@ import "./index.css";
 import Register from "./pages/Register.tsx";
 import Login from "./pages/Login.tsx";
 import {AuthProvider} from "./hooks/UseAuth.tsx";
+import FAQ from "./pages/FAQ.tsx";
 
 const router = createBrowserRouter([
     { path: "/", element: <App /> },
     { path: "/login", element: <Login /> },
     { path: "/register", element: <Register /> },
+    { path: "/faq", element: <FAQ /> },
+    { path: "/about", element: <NotFound /> },
+    { path: "/learn", element: <NotFound /> },
     { path: "*", element: <NotFound /> },
 ]);
 
