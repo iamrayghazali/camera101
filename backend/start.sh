bd53e199546f747020d926a89fe90a84fb226d4f
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-09-26 18:14:58 +0200

    Railway Railpack configuration 11

--- backend/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-python3 -m pip install -r requirements.txt
-python3 manage.py migrate
-python3 manage.py collectstatic --noinput
-gunicorn energetic-charm.wsgi:application --bind 0.0.0.0:$PORT
