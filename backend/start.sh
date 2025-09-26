9a3e997b6ea4c8fff81f8a9e809bf2dbc7a280aa
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-09-26 17:55:16 +0200

    Railway Railpack configuration 9

--- backend/start.sh
+++ backend/start.sh
@@ -3,4 +3,4 @@ set -e
 python3 -m pip install -r requirements.txt
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-gunicorn camera101.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn energetic-charm.wsgi:application --bind 0.0.0.0:$PORT
