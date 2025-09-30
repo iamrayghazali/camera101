83fe7f43ceb914fdacbc783896cbfde4536f87b3
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-09-30 13:41:50 +0200

    Changed navbar register button bg

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -54,7 +54,7 @@ export default function Navbar() {
                   <>
                       <Link
                           to={"/register"}
-                          className={`text-xl font-rama font-bold relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100 ${isHomePage ? 'text-white after:bg-white' : 'hollow-text text-extrabold leaves-bg after:bg-black'}`}
+                          className={`text-xl font-rama font-bold relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100 ${isHomePage ? 'text-white after:bg-white' : 'after:bg-black'}`}
                       >
                           Register
                       </Link>
